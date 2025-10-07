58f00669ca0942ee53a21345a478615135d161c7
Edison Phoon <24FTT2013@student.pb.edu.bn>
2025-10-07 16:49:35 +0800

    Made a WIP Version of Dashboard view page with WIP Blade template

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,11 @@
-@extends('')
+<!-- resources/view/admin/dashboard.blade.php -->
+
+@extends('layouts.admin')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<div class="dashboard">
+    <h2>Welcome back, Admin!</h2>
+</div>
+@endsection
